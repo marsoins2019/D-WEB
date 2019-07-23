4e59033c77a9761309c220657c4ecb7b0809e180
marsouin2019 <51916414+marsouin2019@users.noreply.github.com>
2019-07-23 11:36:13 -0400

    Update form.js

--- COURS/JAVASCRIPT/form.js
+++ COURS/JAVASCRIPT/form.js
@@ -71,13 +71,71 @@ document.getElementById("submit").addEventListener("click", function(event){
     // quantite 1 definition et calcul
     let q1 = document.getElementById("q1");
     //q1.innerHTML = 10;  // ne s'affiche pas dans input q1
-    // div, span, p, li, a, nav, h1-h6, td
-
-    // input renvoi des valeurs = value
-     document.getElementById("q1").value = 10;
-    
+    // div, span, p, li, a, nav, h1-h6, td  // element HTML
 
     // comment faire pour afficher un contenu dans un input ?
+    // input renvoi des valeurs = value
+    document.getElementById("q1").value = 10;
+
+    let  q2 = document.getElementById("q2");
+    q2.value = 25;
+
+    let q3 = document.getElementById("q3");
+    q3.value = 15;
+
+    let q4 = document.getElementById("q4");
+    q4.value = 5;
+
+    let q5 = document.getElementById("q5");
+    q5.value = 8;
+
+    // prix definition et calcul
+    let p1 = document.getElementById("p1");
+    p1.value = 2.50;
+
+    let p2 = document.getElementById("p2");
+    p2.value = 0.50;
+
+    let p3 = document.getElementById("p3");
+    p3.value = 1.25;
+
+    let p4 = document.getElementById("p4");
+    p4.value = 36;
+
+    let p5 = document.getElementById("p5");
+    p5.value = 250.33;
+
+    // calculer quantite/prix et afficher le total ligne
+    /*
+        - variable quantite
+        - variable prix
+        - calcule = quantite *  prix
+        - afficher le total
+
+    */
+    //calculer(q1, p1);   // fonction avec passage de paramettre
+
+    // tableaux - les quantites et les prix 
+    let tabQ = [q1.value,q2.value,q3.value,q4.value,q5.value];
+    let tabP = [p1.value, p2.value, p3.value, p4.value, p5.value];
+
+    calculer(tabQ, tabP);
+
+
+});  // fin fonction anonyme
+
+
+function calculer(quantites, prix)
+{
+    
+    let nbrQ = quantites.length;
+    for(let i=0; i < nbrQ; i++)
+    {
+        console.log(quantites[i]);
+    }
+
+    
+    
 
 
 
@@ -85,6 +143,28 @@ document.getElementById("submit").addEventListener("click", function(event){
 
 
 
+    // calculer ma facture
+    //alert(quantites.value * prix.value);
 
-});
+    // afficher dans totalLine
+    //let totals = document.getElementsByClassName("total");  // collection => tableau
+     
+    // compter le nbr element dans le tableau
+    // permet de recuperer les index
+    //let compteTotal = totals.length;  // affiche 8
+
+
+    // test pour calculer et afficher UN total
+    //let afficheTotal = quantites.value * prix.value;
+
+    // récupuré l'index 0 et affiché le calcul total en html
+    //totals[0].innerHTML = afficheTotal;
+
+
+    
+
+
+
+
+}
 
