7e2da024e3bccdbfaa6b8ad4886a8e7ae75eebb7
marsouin2019 <51916414+marsouin2019@users.noreply.github.com>
2019-08-26 12:07:09 -0400

    Add files via upload

---
+++ COURS/Exercices/locations/locations-tout-genre.com/index.php
@@ -0,0 +1,66 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/all.min.css">
+    <link rel="stylesheet" href="style.css">
+
+
+
+    <title>Locations tout genre</title>
+  </head>
+  <body>
+      <header>
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <h1><span class="cyan">LOCATION</span> FIND <i class="fas fa-map-marker-alt"></i></h1>
+                </div>
+                <div class="col">
+                 <p class="pHeader"> <strong>our network</strong>  <i class="fas fa-square"></i> </p>  
+                </div>
+            </div>
+        </div>
+      </header>
+      <nav class="menu">
+        <div class="container">
+        <div class="row">
+            <div class="col">
+                <ul class="lShop">
+                    <li>
+                        <a href="http://"><i class="fas fa-map-marker-alt"></i> Find Locations</a>
+                    </li>
+                    <li><a href="http://"><i class="fas fa-book-open"></i> Our Species</a></li>
+                    <li><a href="http://"><i class="fas fa-shopping-basket"></i> Shop</a></li>
+                </ul>
+            </div>
+            <div class="col">
+                <ul class="lAccount">
+                    <li><a href="http://"><i class="fas fa-window-restore"></i> My account</a></li>
+                    <li><a href="http://"><i class="fas fa-bookmark"></i> Sign Up</a></li>
+                    <li><a href="http://"><i class="fas fa-envelope"></i> Contact</a></li>
+                </ul>
+            </div>
+            </div>
+        </div>
+
+      </nav>   
+
+    
+    
+
+              
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    
+  </body>
+</html>
