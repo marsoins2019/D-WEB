25f00cf1c33f2d73265543f91d6c8a0913cf9eca
marsouin2019 <51916414+marsouin2019@users.noreply.github.com>
2019-08-29 11:11:35 -0400

    Update index.php

--- COURS/Exercices/locations/locations-tout-genre.com/index.php
+++ COURS/Exercices/locations/locations-tout-genre.com/index.php
@@ -33,17 +33,17 @@
                 <div class="col">
                     <ul class="lShop">
                         <li>
-                            <a href="http://"><i class="fas fa-map-marker-alt"></i> Find Locations</a>
+                            <a href="#"><i class="fas fa-map-marker-alt"></i> Find Locations</a>
                         </li>
-                        <li><a href="http://"><i class="fas fa-book-open"></i> Our Species</a></li>
-                        <li><a href="http://"><i class="fas fa-shopping-basket"></i> Shop</a></li>
+                        <li><a href="#"><i class="fas fa-book-open"></i> Our Species</a></li>
+                        <li><a href="#"><i class="fas fa-shopping-basket"></i> Shop</a></li>
                     </ul>
                 </div>
                 <div class="col">
                     <ul class="lAccount">
-                        <li><a href="http://"><i class="fas fa-window-restore"></i> My account</a></li>
-                        <li><a href="http://"><i class="fas fa-bookmark"></i> Sign Up</a></li>
-                        <li><a href="http://"><i class="fas fa-envelope"></i> Contact</a></li>
+                        <li><a href="#"><i class="fas fa-window-restore"></i> My account</a></li>
+                        <li><a href="#"><i class="fas fa-bookmark"></i> Sign Up</a></li>
+                        <li><a href="#"><i class="fas fa-envelope"></i> Contact</a></li>
                     </ul>
                 </div>
             </div>
@@ -56,8 +56,8 @@
                 </div>
                 <div class="col">
                     <ul class="register">
-                        <li><a href="http://">Login Here or</a></li>
-                        <li><a href="http://" class="btn btn-green">Register</a></li>
+                        <li><a href="#">Login Here or</a></li>
+                        <li><a href="#" class="btn btn-green">Register</a></li>
                     </ul>
                 </div>
             </div>
@@ -74,7 +74,7 @@
                 <button class="btn btn-green">Add Location</button>
             </form>
         </div>
-        <div class="container content">
+        <div class="container">
             <div class="row">
                 <div class="col-2">
                     <div class="btn-group-vertical">
@@ -86,36 +86,137 @@
                     </div>
                 </div>
                 <div class="col-10">
-                    <h2>Locations you've Add</h2>
-                    <p class="pStyl">Lets got some dunny no worries come a bloke. Stands out like a ridgy-didge where lets throw a sickie.</p>
-                    <table class="table table-striped">
-                        <tbody>
-                            <tr>
-                                <td class="strong">Blaz's wichaed ass fishing spot.</td>
-                                <td>Mullet, Autralian Salmon, Travelly</td>
-                                <td>see location <i class="far fa-comment-alt"></i></td>
-                            </tr>
-                            <tr>
-                                <td class="strong">Ball Bag River</td>
-                                <td>Mullet, Autralian Salmon, Travelly</td>
-                                <td>see location <i class="far fa-comment-alt"></i></td>
-                            </tr>
-                            <tr>
-                                <td class="strong">Aireys Inlet - Eagle Rock</td>
-                                <td>Beach east side of Aire River...</td>
-                                <td>see location <i class="far fa-comment-alt"></i></td>
-                            </tr>
-                            <tr>
-                                <td class="strong">Ball Bag River</td>
-                                <td>Mullet, Autralian Salmon, Travelly</td>
-                                <td>see location <i class="far fa-comment-alt"></i></td>
-                            </tr>
-                            
-                        </tbody>
-                   </table>
+                    <div class="container content">
+                        <h2>Locations you've Add</h2>
+                        <p class="pStyl">Lets got some dunny no worries come a bloke. Stands out like a ridgy-didge where lets throw a sickie.</p>
+                        <table class="table table-striped">
+                            <tbody>
+                                <tr>
+                                    <td class="strong">Blaz's wichaed ass fishing spot.</td>
+                                    <td>Mullet, Autralian Salmon, Travelly</td>
+                                    <td class="lLink">see location <i class="far fa-comment-alt"></i></td>
+                                </tr>
+                                <tr>
+                                    <td class="strong">Ball Bag River</td>
+                                    <td>Mullet, Autralian Salmon, Travelly</td>
+                                    <td class="lLink">see location <i class="far fa-comment-alt lGray"></i></td>
+                                </tr>
+                                <tr>
+                                    <td class="strong">Aireys Inlet - Eagle Rock</td>
+                                    <td>Beach east side of Aire River...</td>
+                                    <td class="lLink">see location <i class="far fa-comment-alt"></i></td>
+                                </tr>
+                                <tr>
+                                    <td class="strong">Ball Bag River</td>
+                                    <td>Mullet, Autralian Salmon, Travelly</td>
+                                    <td class="lLink">see location <i class="far fa-comment-alt lGray"></i></td>
+                                </tr>
+                                
+                            </tbody>
+                        </table>
+                        <hr>
+
+                        <h2>Recent Locations you've rated</h2>
+                        <p class="pStyl">Lets got some dunny no worries come a bloke. Stands out like a ridgy-didge where lets throw a sickie.</p>
+                        <table class="table table-striped">
+                            <tbody>
+                                <tr>
+                                    <td class="strong">Blaz's wichaed ass fishing spot.</td>
+                                    <td class="lFont">Mullet, Autralian Salmon, Travelly</td>
+                                    <td><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i></td>
+                                    <td class="lLinkb lFont">View Location on Map</td>
+                                </tr>
+                                <tr>
+                                    <td class="strong">Ball Bag River</td>
+                                    <td class="lFont">Mullet, Autralian Salmon, Travelly</td>
+                                    <td><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i></td>
+                                    <td class="lLinkb lFont">View Location on Map</td>
+                                </tr>
+                                <tr class="tdBorder">
+                                    <td class="strong">Aireys Inlet - Eagle Rock</td>
+                                    <td class="lFont">Beach east side of Aire River...</td>
+                                    <td><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i></td>
+                                    <td class="lLinkb lFont">View Location on Map</td>
+                                </tr>
+                                <tr>
+                                    <td class="strong">Ball Bag River</td>
+                                    <td class="lFont">Mullet, Autralian Salmon, Travelly</td>
+                                    <td><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i></td>
+                                    <td class="lLinkb lFont">View Location on Map</td>
+                                </tr>
+                                <tr class="tdBorder">
+                                    <td class="strong">Aireys Inlet - Eagle Rock</td>
+                                    <td class="lFont">Beach east side of Aire River...</td>
+                                    <td><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i></td>
+                                    <td class="lLinkb lFont">View Location on Map</td>
+                                </tr>
+                                
+                            </tbody>
+                        </table>
+
+
+
+
+                </div>
+                
+
+                    
                 </div>
             </div>
         </div>
+        <footer>
+        <nav class="menu">
+            <div class="container">
+                <div class="row">
+                    <div class="col-2">
+                        <h3>About us</h3>
+                        <ul>
+                            <li><a href="#">About us</a></li>
+                            <li><a href="#">Who started it</a></li>
+                            <li><a href="#">How to help</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-3">
+                        <h3>Account Informations</h3>
+                        <ul>
+                            <li><a href="#">How to login</a></li>
+                            <li><a href="#">Create an account</a></li>
+                            <li><a href="#">Login</a></li>
+                            <li><a href="#">Join us</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-2">
+                        <h3>About us</h3>
+                        <ul>
+                            <li><a href="#">About us</a></li>
+                            <li><a href="#">Who started it</a></li>
+                            <li><a href="#">How to help</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-3">
+                        <h3>Account Informations</h3> 
+                        <ul>
+                            <li><a href="#">How to login</a></li>
+                            <li><a href="#">Create an account</a></li>
+                            <li><a href="#">Login</a></li>
+                            <li><a href="#">Join us</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-2">
+                        <h3>Ours Networks <span class="badge badge-secondary"><i class="fas fa-heart"></i></span></h3>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-9">
+                    <p>We've made some small changes to BlazRobar.com. To keep the quality hight on the website and make sure everyone is playing nice. We've added a member login section. Created an account, then you'll have access to dowload all the files on the site. Don't worry through, it's all still free. </p>
+                    </div>
+                    <div class="col-3"></div>
+                </div>
+                
+            </div>
+        </nav>
+
+        </footer>
 
 
 
@@ -123,31 +224,7 @@
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-      
-     
-
-    
-    
-
-              
-
-    <!-- Optional JavaScript -->
+ <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
@@ -155,3 +232,4 @@
     
   </body>
 </html>
+
