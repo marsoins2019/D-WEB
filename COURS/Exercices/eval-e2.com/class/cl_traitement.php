f7112a0ea3c2cc122bcb3957171f18b4e5350a45
marsouin2019 <51916414+marsouin2019@users.noreply.github.com>
2019-10-08 14:58:45 -0400

    Update cl_traitement.php

--- COURS/Exercices/eval-e2.com/class/cl_traitement.php
+++ COURS/Exercices/eval-e2.com/class/cl_traitement.php
@@ -33,12 +33,19 @@ class Personne
 
         // mise en forme et securite password
         $this->civilite = $data["frmCivilite"];
+        if( $this->civilite != "M" || $this->civilite != "Mme" )
+        {
+            return false;
+        }
+
         $this->prenom = ltrim(htmlentities(ucfirst($data["frmPrenom"])));
         $this->nom = ltrim(htmlentities(strtoupper($data["frmNom"])));
 
         $this->adresse = ltrim(htmlentities($data["frmAdresse"]));
         $this->complementaire = ltrim(htmlentities($data["frmComplementaire"]));
         $this->postal = ltrim(htmlentities($data["frmPostal"]));
+        
+
         
         $this->ville = ltrim(htmlentities(ucfirst($data["frmVille"])));
         $this->telephone = ltrim(htmlentities($data["frmPhone"]));
@@ -47,14 +54,21 @@ class Personne
 
         // mise en securite du mot de passe
 
+        // condition test longueur chaine mot de passe
+        if( strlen($data["frmPass"]) < 8 )
+        {
+            return false;
 
-        $this->password = password_hash($data["frmPass"], PASSWORD_DEFAULT);
-
-
-        
-
+        } else {
+            if( strlen($data["frmPass"]) > 12)
+            {
+                return false;
+            } else {
+                $this->password = password_hash($data["frmPass"], PASSWORD_DEFAULT);
+            }
+        }
        
-
+        
 
     }
 
