eeb35e11231d8b7522aec6710817788f11ccc8ab
marsouin2019 <51916414+marsouin2019@users.noreply.github.com>
2019-11-20 13:33:48 -0400

    Update login.php

--- COURS/Exercices/eval-e2.com/login.php
+++ COURS/Exercices/eval-e2.com/login.php
@@ -48,8 +48,48 @@
                         
                         <button type="submit" class="btn btn-primary">Login</button>
                     </form> 
+        
+        <!-- Button trigger modal -->
+<div class="container">
+    <p class="info"><a href="#" data-target="#exampleModalCenter" data-toggle="modal">Forgot my password</a></p>
+</div>
+
+<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle"
+    aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h2 class="modal-title" id="exampleModalLongTitle">What's My Password?</h2>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <p>If you have forgotten your password you can reset it here.</p>
+                <div class="panel-body">
+                  <form action="traitement.php" method="post" name="frmPass" id="frmPass">
+                    <fieldset>
+                        <div class="form-group">
+                            <input class="form-control input-lg" placeholder="E-mail Address" name="frmEmail" type="email" required>
+                        </div>
+                        <button type="submit" class="btn btn-lg btn-primary btn-block" name="frmForm" value="frmPass">Send My Password</button>
+                    </fieldset>
+                  </form>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+<!-- Modal -->
     </div>
   </section>
+   
 
   <?php
     include("includes/footer.php");
